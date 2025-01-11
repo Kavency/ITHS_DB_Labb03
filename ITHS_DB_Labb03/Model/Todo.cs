8f9853b613efbff58587366ab407dcb85df44feb
llavner <marcus.renvall@gmail.com>
2025-01-11 15:43:03 +0100

    Got a conflict while trying to add updated Todo.cs

---
+++ ITHS_DB_Labb03/Model/Todo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITHS_DB_Labb03.Model
+{
+    class Todo
+    {
+        // Id - String Guid
+        // Datum och tid skapad - DateTime
+        // Titel - String
+        // Beskrivning - String
+        // Delat med användare - Lista med strings(GUID)
+        // Datum och tid avklarad - DateTime
+        // Avklarad - Bool
+        // Taggar - Lista<Tags>
+        // Stjärnmärkning - Bool
+
+        public string Id { get; set; }
+        public DateTime StartTodo { get; set; }
+        public string Title { get; set; }
+        public string Discription { get; set; }
+        public List<string> SharedTodos { get; set; }
+        public DateTime EndTodo { get; set; }
+        public bool IsDone { get; set; }
+        public List<Tag> Tags { get; set; }
+        public bool IsStared { get; set; }
+    }
+}
