7b790ccb1f2c9ef7a57b4252008675f6b84769c4
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-01-11 14:17:49 +0100

    Add Todo model.

---
+++ ITHS_DB_Labb03/Model/Todo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITHS_DB_Labb03.Model
+{
+    class Todo
+    {
+        // Id - String Guid
+        // Datum och tid skapad - DateTime
+        // Titel - String
+        // Beskrivning - String
+        // Delat med användare - Lista med strings(GUID)
+        // Datum och tid avklarad - DateTime
+        // Avklarad - Bool
+        // Taggar - Lista<Tags>
+        // Stjärnmärkning - Bool
+    }
+}
