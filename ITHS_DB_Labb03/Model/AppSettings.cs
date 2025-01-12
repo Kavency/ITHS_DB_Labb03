bcecc3e83964f84617dd7f056448e6f53f3b505d
llavner <marcus.renvall@gmail.com>
2025-01-12 15:32:50 +0100

    Added modelclasses

---
+++ ITHS_DB_Labb03/Model/AppSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITHS_DB_Labb03.Model
+{
+    internal class AppSettings
+    {
+        public User CurrentUser { get; set; }
+        public TodoCollection SelectedCollection { get; set; }
+
+    }
+}
