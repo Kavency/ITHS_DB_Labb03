18bf188aa6570c6237dc6cb68af72620c086d6fb
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-01-13 12:08:33 +0100

    Change properties for User class. This is just a temporarly change while making sure we can work with the database.

--- ITHS_DB_Labb03/Model/User.cs
+++ ITHS_DB_Labb03/Model/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,12 +10,12 @@ namespace ITHS_DB_Labb03.Model
     internal class User
     {
 
-        public int UserId { get; set; }
-        public string UserName { get; set; }
-        public string FullName { get; set; }
-        public string Email { get; set; }
-        public DateTime UserCreated { get; set; }
-        public List<Todo> Todos { get; set; }
+        public ObjectId Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        //public string Email { get; set; }
+        //public DateTime UserCreated { get; set; }
+        //public List<Todo> Todos { get; set; }
 
     }
 }
