f70ec425b568c8db8eb605a0048efccba71733c0
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-01-13 10:26:45 +0100

    Add main list view.

---
+++ ITHS_DB_Labb03/View/ListView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ITHS_DB_Labb03.View
+{
+    /// <summary>
+    /// Interaction logic for ListView.xaml
+    /// </summary>
+    public partial class ListView : UserControl
+    {
+        public ListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
