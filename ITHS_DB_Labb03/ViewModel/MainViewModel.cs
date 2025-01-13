ffb47bfbd132a7073475d7c845ad54ed87aea567
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-01-13 12:00:54 +0100

    Add GetUsers method.

--- ITHS_DB_Labb03/ViewModel/MainViewModel.cs
+++ ITHS_DB_Labb03/ViewModel/MainViewModel.cs
@@ -7,6 +7,10 @@ using System.Threading.Tasks;
 namespace ITHS_DB_Labb03.ViewModel
 {
     internal class MainViewModel
+        private void GetUsers()
     {
+            using var db = new TodoDbContext();
+            var result = db.Users.ToList();
+            Users = new ObservableCollection<User>(result);
     }
 }
