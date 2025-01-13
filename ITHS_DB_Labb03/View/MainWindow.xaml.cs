a40a2ef1d4c49f96a78ca9683a1885a5847bc19a
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-01-13 12:06:21 +0100

    Add DataContext.

--- ITHS_DB_Labb03/View/MainWindow.xaml.cs
+++ ITHS_DB_Labb03/View/MainWindow.xaml.cs
@@ -1,16 +1,17 @@
-﻿
-using ITHS_DB_Labb03.Core;
+﻿using ITHS_DB_Labb03.Core;
+using ITHS_DB_Labb03.ViewModel;
 using System.Diagnostics;
 using System.Windows;
 
 namespace ITHS_DB_Labb03;
 
-    public partial class MainWindow : Window
+public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
 
+            DataContext = new MainViewModel();
 
             using (var db = new TodoDbContext())
             {
