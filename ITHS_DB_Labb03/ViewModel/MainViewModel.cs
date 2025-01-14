15d90a3e97436e2665f7916e512b3963223452b6
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-01-14 19:31:33 +0100

    Move all relavant user related to the UserViewModel

--- ITHS_DB_Labb03/ViewModel/MainViewModel.cs
+++ ITHS_DB_Labb03/ViewModel/MainViewModel.cs
@@ -8,73 +8,19 @@ namespace ITHS_DB_Labb03.ViewModel
 {
     internal class MainViewModel : VMBase
     {
-        private User _currentUser;
+        private UserViewModel _userViewModel;
         private Visibility _listViewVisibility;
-        private Visibility _userViewVisibility;
-        private Visibility _userDetailsVisibility;
-        public UserViewModel userViewModel;
 
-        public User UserDetails { get; set; } = new User();
-        public User CurrentUser { get => _currentUser; set { _currentUser = value; OnPropertyChanged(); } }
+        public UserViewModel UserViewModel { get =>_userViewModel; set { _userViewModel = value; OnPropertyChanged(); } }
         public Visibility ListViewVisibility { get => _listViewVisibility; set { _listViewVisibility = value; OnPropertyChanged(); } }
-        public Visibility UserViewVisibility { get => _userViewVisibility; set { _userViewVisibility = value; OnPropertyChanged(); } }
-        public Visibility UserDetailsVisibility { get => _userDetailsVisibility; set { _userDetailsVisibility = value; OnPropertyChanged(); } }
-        public ObservableCollection<User> Users { get; set; }
-        public RelayCommand SetCurrentUserCMD { get; }
-        public RelayCommand ShowUserDetailsCMD { get; }
-        public RelayCommand ShowUsersCMD { get; }
-        public RelayCommand AddNewUserCMD { get; }
-        public RelayCommand DeleteUserCMD { get; }
-        public RelayCommand EditUserCMD { get; }
-        public RelayCommand CancelNewUserCMD { get; }
 
 
         public MainViewModel()
         {
-            userViewModel = new UserViewModel(this);
-
-            SetCurrentUserCMD = new RelayCommand(SetCurrentUser);
-            ShowUserDetailsCMD = new RelayCommand(ShowUserDetails);
-            ShowUsersCMD = new RelayCommand(ShowUsers);
-            AddNewUserCMD = new RelayCommand(AddUser);
-            DeleteUserCMD = new RelayCommand(DeleteUser);
-            EditUserCMD = new RelayCommand(EditUser);
-            CancelNewUserCMD = new RelayCommand(CancelButtonPressed);
+            UserViewModel = new UserViewModel(this);
 
             GetUsersFromDb();
             CheckUserCollection();
-        }
-
-
-        private void ShowUsers(object obj)
-        {
-            ChangeView("userview");
-        }
-
-
-        private void CancelButtonPressed(object obj)
-        {
-            UserDetails = new();
-            OnPropertyChanged(nameof(UserDetails));
-            ChangeView("userview");
-        }
-
-
-        private void ShowUserDetails(object obj)
-        {
-            ChangeView("userdetails");
-
-            if(obj is User)
-            {
-                UserDetails = obj as User;
-                OnPropertyChanged(nameof(UserDetails));
-                // Update button visibility
-            }
-            else if(obj is null)
-            {
-                // Save button vivibility
-            }
-
         }
 
 
@@ -82,98 +28,37 @@ namespace ITHS_DB_Labb03.ViewModel
         {
             using var db = new TodoDbContext();
             var result = db.Users.ToList();
-            Users = new ObservableCollection<User>(result);
-        }
-
-
-        private void SetCurrentUser(object obj)
-        {
-            if (obj is not null)
-            {
-                CurrentUser = new User();
-                CurrentUser = obj as User;
-            }
-        }
-
-
-        private void AddUser(object obj)
-        {
-            UserDetails.Id = ObjectId.GenerateNewId();
-            UserDetails.UserCreated = DateTime.Now;
-            UserDetails.TodoCollections = new ObservableCollection<TodoCollection>();
-
-            using var db = new TodoDbContext();
-
-            Users.Add(UserDetails);
-            db.Users.Add(UserDetails);
-            db.SaveChanges();
-
-            CancelButtonPressed(obj);
-        }
-
-
-        private void EditUser(object obj)
-        {
-            using var db = new TodoDbContext();
-
-            var userToUpdate = db.Users.FirstOrDefault(u => u.Id == CurrentUser.Id);
-
-            if(userToUpdate != null)
-            {
-                userToUpdate.FirstName = CurrentUser.FirstName;
-                userToUpdate.LastName = CurrentUser.LastName;
-                userToUpdate.UserName = CurrentUser.UserName;
-                userToUpdate.Email = CurrentUser.Email;
-                
-                db.Users.Update(userToUpdate);
-                db.SaveChanges();
-                OnPropertyChanged(nameof(Users));
-            }
-        }
-
-
-        private void DeleteUser(object obj)
-        {
-            using var db = new TodoDbContext();
-
-            var selectedUser = db.Users.FirstOrDefault(u => u.Id == CurrentUser.Id);
-
-            if (selectedUser != null)
-            {
-                Users.Remove(selectedUser);
-                db.Users.Remove(selectedUser);
-                db.SaveChanges();
-            }
+            UserViewModel.Users = new ObservableCollection<User>(result);
         }
 
 
         private void CheckUserCollection()
         {
-            if(Users is null || Users.Count == 0)
+            if(UserViewModel.Users is null || UserViewModel.Users.Count == 0)
                 ChangeView("userview");
             else
                 ChangeView("listview");
         }
 
 
-        private void ChangeView(string view)
+        internal void ChangeView(string view)
         {
             if (view.ToLower() == "userview")
             {
-                UserViewVisibility = Visibility.Visible;
-                UserDetailsVisibility = Visibility.Hidden;
+                UserViewModel.UserViewVisibility = Visibility.Visible;
+                UserViewModel.UserDetailsVisibility = Visibility.Hidden;
                 ListViewVisibility = Visibility.Hidden;
             }
             else if(view.ToLower() == "userdetails")
             {
-                UserViewVisibility = Visibility.Hidden;
-                UserDetailsVisibility = Visibility.Visible;
+                UserViewModel.UserViewVisibility = Visibility.Hidden;
+                UserViewModel.UserDetailsVisibility = Visibility.Visible;
                 ListViewVisibility = Visibility.Hidden;
             }
             else if(view.ToLower() == "listview")
             {
-                UserViewVisibility = Visibility.Hidden;
-                UserDetailsVisibility = Visibility.Hidden;
+                UserViewModel.UserViewVisibility = Visibility.Hidden;
+                UserViewModel.UserDetailsVisibility = Visibility.Hidden;
                 ListViewVisibility = Visibility.Visible;
             }
         }
--- ITHS_DB_Labb03/ViewModel/UserViewModel.cs
+++ ITHS_DB_Labb03/ViewModel/UserViewModel.cs
@@ -1,21 +1,138 @@
 ﻿using ITHS_DB_Labb03.Core;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ITHS_DB_Labb03.Model;
+using MongoDB.Bson;
+using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace ITHS_DB_Labb03.ViewModel
 {
     internal class UserViewModel : VMBase
     {
-        private readonly MainViewModel mainViewModel;
+        private MainViewModel _mainViewModel;
+        private User _userDetails = new User();
+        private User _currentUser;
+        private Visibility _userViewVisibility;
+        private Visibility _userDetailsVisibility;
+
+        public MainViewModel MainViewModel { get => _mainViewModel; set { _mainViewModel = value; OnPropertyChanged(); } }
+        public User UserDetails { get => _userDetails; set { _userDetails = value; OnPropertyChanged(); } }
+        public User CurrentUser { get => _currentUser; set { _currentUser = value; OnPropertyChanged(); } }
+        public ObservableCollection<User> Users { get; set; }
+        public Visibility UserViewVisibility { get => _userViewVisibility; set { _userViewVisibility = value; OnPropertyChanged(); } }
+        public Visibility UserDetailsVisibility { get => _userDetailsVisibility; set { _userDetailsVisibility = value; OnPropertyChanged(); } }
+
+
+        public RelayCommand ShowUsersCMD { get; }
+        public RelayCommand ShowUserDetailsCMD { get; }
+        public RelayCommand AddNewUserCMD { get; }
+        public RelayCommand DeleteUserCMD { get; }
+        public RelayCommand EditUserCMD { get; }
+        public RelayCommand CancelNewUserCMD { get; }
+        public RelayCommand SetCurrentUserCMD { get; }
+
 
         public UserViewModel(MainViewModel mainViewModel)
         {
-            this.mainViewModel = mainViewModel;
+            MainViewModel = mainViewModel;
+
+            ShowUsersCMD = new RelayCommand(ShowUsers);
+            ShowUserDetailsCMD = new RelayCommand(ShowUserDetails);
+            AddNewUserCMD = new RelayCommand(AddUser);
+            DeleteUserCMD = new RelayCommand(DeleteUser);
+            EditUserCMD = new RelayCommand(EditUser);
+            CancelNewUserCMD = new RelayCommand(CancelButtonPressed);
+            SetCurrentUserCMD = new RelayCommand(SetCurrentUser);
+
+        }
 
 
+        private void ShowUsers(object obj)
+        {
+            MainViewModel.ChangeView("userview");
+        }
+
+
+        private void ShowUserDetails(object obj)
+        {
+            MainViewModel.ChangeView("userdetails");
+
+            if (obj is User)
+            {
+                UserDetails = obj as User;
+                OnPropertyChanged(nameof(UserDetails));
+                // Update button visibility
+            }
+            else if (obj is null)
+            {
+                // Save button vivibility
+            }
+
+        }
+
+        private void AddUser(object obj)
+        {
+            UserDetails.Id = ObjectId.GenerateNewId();
+            UserDetails.UserCreated = DateTime.Now;
+            UserDetails.TodoCollections = new ObservableCollection<TodoCollection>();
+
+            using var db = new TodoDbContext();
+
+            Users.Add(UserDetails);
+            db.Users.Add(UserDetails);
+            db.SaveChanges();
+
+            CancelButtonPressed(obj);
+        }
+
+
+        private void EditUser(object obj)
+        {
+            using var db = new TodoDbContext();
+
+            var userToUpdate = db.Users.FirstOrDefault(u => u.Id == CurrentUser.Id);
+
+            if (userToUpdate != null)
+            {
+                userToUpdate.FirstName = CurrentUser.FirstName;
+                userToUpdate.LastName = CurrentUser.LastName;
+                userToUpdate.UserName = CurrentUser.UserName;
+                userToUpdate.Email = CurrentUser.Email;
+
+                db.Users.Update(userToUpdate);
+                db.SaveChanges();
+                OnPropertyChanged(nameof(Users));
+            }
+        }
+
+
+        private void DeleteUser(object obj)
+        {
+            using var db = new TodoDbContext();
+
+            var selectedUser = db.Users.FirstOrDefault(u => u.Id == CurrentUser.Id);
+
+            if (selectedUser != null)
+            {
+                Users.Remove(selectedUser);
+                db.Users.Remove(selectedUser);
+                db.SaveChanges();
+            }
+        }
+
+        private void CancelButtonPressed(object obj)
+        {
+            UserDetails = new();
+            OnPropertyChanged(nameof(UserDetails));
+            MainViewModel.ChangeView("userview");
+        }
+
+        private void SetCurrentUser(object obj)
+        {
+            if (obj is not null)
+            {
+                CurrentUser = new User();
+                CurrentUser = obj as User;
+            }
         }
     }
 }
