80a17dc71f2ba735da9b021cfaf9f8e2818a4572
llavner <marcus.renvall@gmail.com>
2025-01-16 05:44:25 +0100

    Created new class TodoCollectionViewModel with constructor.

---
+++ ITHS_DB_Labb03/ViewModel/TodoCollectionViewModel.cs
@@ -0,0 +1,25 @@
+﻿using ITHS_DB_Labb03.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITHS_DB_Labb03.ViewModel
+{
+    internal class TodoCollectionViewModel : VMBase
+    {
+		private MainViewModel _mainViewModel;
+
+		public MainViewModel MainViewModel { get => _mainViewModel; set { _mainViewModel = value; OnPropertyChanged(); } }
+
+
+        public TodoCollectionViewModel(MainViewModel mainViewModel)
+        {
+
+            MainViewModel = mainViewModel;
+
+        }
+
+    }
+}
