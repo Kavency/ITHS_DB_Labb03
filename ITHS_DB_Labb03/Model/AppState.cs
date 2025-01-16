d846a3384edbbd4882cfcf5165946b06ed161998
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-01-16 20:40:21 +0100

    Add window properties.

--- ITHS_DB_Labb03/Model/AppState.cs
+++ ITHS_DB_Labb03/Model/AppState.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows;
 
 namespace ITHS_DB_Labb03.Model
 {
     internal class AppState
     {
+        public WindowState State { get; set; }
+        public double WindowTop { get; set; }
+        public double WindowLeft { get; set; }
+        public double WindowHeight { get; set; }
+        public double WindowWidth { get; set; }
         public User CurrentUser { get; set; }
         public TodoCollection SelectedCollection { get; set; }
-
     }
 }
