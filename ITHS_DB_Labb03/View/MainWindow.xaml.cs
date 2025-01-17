7a5fb1aa093ab899dcdf467d122938d6fc97e917
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-01-17 08:25:05 +0100

    Add Load and save window state.

--- ITHS_DB_Labb03/View/MainWindow.xaml.cs
+++ ITHS_DB_Labb03/View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using ITHS_DB_Labb03.Core;
+using ITHS_DB_Labb03.Model;
 using ITHS_DB_Labb03.ViewModel;
 using System.Diagnostics;
 using System.Windows;
@@ -6,18 +7,63 @@ using System.Windows;
 namespace ITHS_DB_Labb03;
 
 public partial class MainWindow : Window
+{
+    internal AppState AppState { get; set; }
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+        DataContext = new MainViewModel();
+        AppState = new AppState();
+        EnsureCreated();
+        LoadAppState();
+    }
+
+
+    private static void EnsureCreated()
+    {
+        using var db = new TodoDbContext();
+
+        db.Database.EnsureCreated();
+        Debug.WriteLine($"Database connection: {db.Database.CanConnect()}");
+    }
+
+
+    private void LoadAppState()
+    {
+        using var db = new TodoDbContext();
+        var state = db.AppState.FirstOrDefault();
+        AppState = state;
+
+        if (state is not null)
         {
-            InitializeComponent();
-
-            DataContext = new MainViewModel();
-
-            using (var db = new TodoDbContext())
-            {
-                db.Database.EnsureCreated();
-
-                Debug.WriteLine($"Database connection: {db.Database.CanConnect()}");
-            }
+            this.WindowState = state.WindowState;
+            this.Top = state.WindowTop;
+            this.Left = state.WindowLeft;
+            this.Width = state.WindowWidth;
+            this.Height = state.WindowHeight;
         }
     }
+
+
+    private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+    {
+        AppState.WindowState = this.WindowState;
+        AppState.WindowTop = this.Top;
+        AppState.WindowLeft = this.Left;
+        AppState.WindowWidth = this.Width;
+        AppState.WindowHeight = this.Height;
+
+        using var db = new TodoDbContext();
+
+        var documentCount = db.AppState.Count();
+
+        if(documentCount > 0)
+        {
+            var allDocuments = db.AppState.ToList();
+            db.AppState.RemoveRange(allDocuments);
+            db.SaveChanges();
+        }
+        db.AppState.Add(AppState);
+        db.SaveChanges();
+    }
+}
