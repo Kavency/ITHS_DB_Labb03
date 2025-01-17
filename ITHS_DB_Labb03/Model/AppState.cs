576eb9200028f479802d1c6ceb6a108893cac1fb
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-01-17 08:24:26 +0100

    Add properties to AppState class

--- ITHS_DB_Labb03/Model/AppState.cs
+++ ITHS_DB_Labb03/Model/AppState.cs
@@ -1,15 +1,17 @@
-﻿using System.Windows;
+﻿using MongoDB.Bson;
+using System.Windows;
 
 namespace ITHS_DB_Labb03.Model
 {
     internal class AppState
     {
-        public WindowState State { get; set; }
+        public ObjectId Id { get; set; }
+        public WindowState WindowState { get; set; }
         public double WindowTop { get; set; }
         public double WindowLeft { get; set; }
         public double WindowHeight { get; set; }
         public double WindowWidth { get; set; }
-        public User CurrentUser { get; set; }
-        public TodoCollection SelectedCollection { get; set; }
+        //public User CurrentUser { get; set; }
+        //public TodoCollection SelectedCollection { get; set; }
     }
 }
