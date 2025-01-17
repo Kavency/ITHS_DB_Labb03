be54203f9d6a214c9b1e9f340a6f5d5443faa121
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-01-17 08:23:32 +0100

    Add DbSet for AppState

--- ITHS_DB_Labb03/Core/TodoDbContext.cs
+++ ITHS_DB_Labb03/Core/TodoDbContext.cs
@@ -1,27 +1,20 @@
 ﻿using ITHS_DB_Labb03.Model;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ITHS_DB_Labb03.Core;
 
 internal class TodoDbContext : DbContext
 {
-
     public DbSet<Todo> Todos { get; set; }
     public DbSet<User> Users { get; set; }
     public DbSet<Tag> Tags { get; set; }
     public DbSet<TodoCollection> TodoCollections { get; set; }
+    public DbSet<AppState> AppState { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-
         var connectionString = "mongodb://localhost:27017/";
         var dbName = "todoapp";
-
 
         optionsBuilder.UseMongoDB(connectionString, dbName);
     }
