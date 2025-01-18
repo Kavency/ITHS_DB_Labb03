8162040b03e67139712293daa8107ffdaae4ddaf
llavner <marcus.renvall@gmail.com>
2025-01-18 08:27:28 +0100

    Added ObservebleCollections, marked out one.

--- ITHS_DB_Labb03/ViewModel/TodoCollectionViewModel.cs
+++ ITHS_DB_Labb03/ViewModel/TodoCollectionViewModel.cs
@@ -1,16 +1,21 @@
 ﻿using ITHS_DB_Labb03.Core;
+using ITHS_DB_Labb03.Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ITHS_DB_Labb03.ViewModel
-{
+namespace ITHS_DB_Labb03.ViewModel;
+
     internal class TodoCollectionViewModel : VMBase
     {
 		private MainViewModel _mainViewModel;
+    public ObservableCollection<Todo> Todos { get; set; }
+    public ObservableCollection<TodoCollection> TodoCollection { get; set; }
 
+    //public ObservableCollection<User> Users { get; set; } ?
 		public MainViewModel MainViewModel { get => _mainViewModel; set { _mainViewModel = value; OnPropertyChanged(); } }
 
 
