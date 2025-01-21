e26db2282a1d46d9e5a58dad60cea7b0d3ac7ea2
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2025-01-21 19:21:45 +0100

    Add EditListView view

---
+++ ITHS_DB_Labb03/View/EditListView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace ITHS_DB_Labb03.View
+{
+    public partial class EditListView : UserControl
+    {
+        public EditListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
